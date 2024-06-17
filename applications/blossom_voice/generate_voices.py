4a85a642fd0a91509907450f0db00637492b3d18
Nathan Dennler <dennler@usc.edu>
2024-06-17 13:04:15 -0700

    updates the scripts to use the voice ids to generate speech

--- applications/blossom_voice/generate_voices.py
+++ applications/blossom_voice/generate_voices.py
@@ -1,7 +1,34 @@
 import torch
 from TTS.api import TTS
+from TTS.tts.configs.xtts_config import XttsConfig
+from TTS.tts.models.xtts import Xtts
 import os
 from tqdm import tqdm
+import pickle
+import torchaudio
 
+embeds = pickle.load(open('./data/speaker_info.pkl', 'rb'))
+
+print("Loading model...")
+config = XttsConfig()
+config.load_json("./xtts_v2/config.json")
+model = Xtts.init_from_config(config)
+model.load_checkpoint(config, checkpoint_dir="./xtts_v2")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+
+for key, vals in tqdm(embeds.items()):
+    gpt_cond_latent, speaker_embedding = vals
+
+    out = model.inference(
+        "Hi! my name is blossom, and I will be your mindfulness companion.",
+        "en",
+        gpt_cond_latent,
+        speaker_embedding,
+        temperature=0.7, # Add custom parameters here
+    )
+    torchaudio.save(f"./data/generated_voice/{key[:-4]}.wav", torch.tensor(out["wav"]).unsqueeze(0), 24000)
 
     
